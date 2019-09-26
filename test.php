19797cfce3344a985e2ad74a3565d2801873c617
surajkrsingh <suraj.sk243@gmail.com>
2019-09-26 14:16:22 +0530

    Remove unnecessary files

--- test.php
+++ test.php
@@ -13,7 +13,7 @@
  * Version:     1.0.0
  * Author:      Surajkumar Singh
  * Author URI:  https://profiles.wordpress.org/surajkumarsingh/
- * Text Domain: wp-book
+ * Text Domain: test
  * License:     GPL-2.0+
  * License URI: http://www.gnu.org/licenses/gpl-2.0.txt
  * Domain Path: /languages
